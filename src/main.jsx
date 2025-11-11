2bbd29dccc2cd1367aaea34eda77ab8f90c09122
cionnamacgp-tech <cionnamacgp@gmail.com>
2025-11-11 13:52:04 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import FodFilms from "./App";
-import AboutPage from "./AboutPage";
-import WorkPage from "./WorkPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import FodFilms from "./App.jsx";
+import AboutPage from "./AboutPage.jsx";
+import WorkPage from "./WorkPage.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<FodFilms />} />
-        <Route path="/about" element={<AboutPage />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+  <Router>
+    <Routes>
+      <Route path="/" element={<FodFilms />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/work" element={<WorkPage />} />
+    </Routes>
+  </Router>
 );
