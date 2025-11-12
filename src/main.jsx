ba243508f719e28618f96bddb7623e5c7f918c17
cionnamacgp-tech <cionnamacgp@gmail.com>
2025-11-12 13:24:01 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ScrollToTop from "./ScrollToTop";
 import FodFilms from "./App";
 import AboutPage from "./AboutPage";
 import WorkPage from "./WorkPage";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <ScrollToTop />
-    <Routes>
-      <Route path="/" element={<FodFilms />} />
-      <Route path="/about" element={<AboutPage />} />
-      <Route path="/work" element={<WorkPage />} />
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<FodFilms />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/work" element={<WorkPage />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
