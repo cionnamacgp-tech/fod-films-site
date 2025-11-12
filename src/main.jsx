75ed336b3aa764a4c576e78f45e422c074c9bf9f
cionnamacgp-tech <cionnamacgp@gmail.com>
2025-11-12 13:01:49 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import FodFilms from "./App.jsx";
-import AboutPage from "./AboutPage.jsx";
-import WorkPage from "./WorkPage.jsx";
-import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ScrollToTop from "./ScrollToTop";
+import FodFilms from "./App";
+import AboutPage from "./AboutPage";
+import WorkPage from "./WorkPage";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Router>
+  <BrowserRouter>
+    <ScrollToTop />
     <Routes>
       <Route path="/" element={<FodFilms />} />
       <Route path="/about" element={<AboutPage />} />
-<Route path="/work" element={<WorkPage />} />
+      <Route path="/work" element={<WorkPage />} />
     </Routes>
-  </Router>
+  </BrowserRouter>
 );
